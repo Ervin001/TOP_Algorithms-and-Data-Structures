77e57d446bfeeb28b3c6ce6ce4c25ab3d4d75954
Ervin Gonzalez <83973082+Ervin001@users.noreply.github.com>
2024-02-03 22:55:18 -0600

    Created a basic hash function

---
+++ hashMap/main.js
@@ -0,0 +1,16 @@
+function stringToNumber(string) {
+  let hashCode = 0;
+
+  const primeNumber = 31;
+  for (let i = 0; i < string.length; i++) {
+    hashCode = primeNumber * hashCode + string.charCodeAt(i);
+  }
+
+  return hashCode;
+}
+
+function hash(name, surname) {
+  return stringToNumber(name) % 16;
+}
+
+console.log(hash('Manon'));
